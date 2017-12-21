02c73f6d5a0bc2400848d60cede0eae80f99f645
Reuben Bond <a-rebond@microsoft.com>
2017-12-22 02:40:47 +1100

    more plane coding but seems like a dll clash in msbuild process

--- src/Hagar.CodeGenerator.MSBuild/GenerateHagarCode.cs
+++ src/Hagar.CodeGenerator.MSBuild/GenerateHagarCode.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Runtime.InteropServices;
+using Buildalyzer;
 using Microsoft.Build.Framework;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.VisualBasic;
 using MSBuildTask = Microsoft.Build.Utilities.Task;
 
 namespace Hagar.CodeGenerator.MSBuild
 {
     public class GenerateHagarCode : MSBuildTask
     {
+        public string ProjectPath { get; set; }
+        public string ProjectGuid { get; set; }
+        public string OutputType { get; set; }
+        public string TargetPath { get; set; }
         public string[] Compile { get; set; }
         public string[] Reference { get; set; }
 
@@ -32,7 +43,93 @@ namespace Hagar.CodeGenerator.MSBuild
                 }
             }
 
+
+            string projectName = Path.GetFileNameWithoutExtension(ProjectPath);
+            ProjectId projectId = !string.IsNullOrEmpty(ProjectGuid)
+                                  && Guid.TryParse(ProjectGuid, out var projectIdGuid)
+                ? ProjectId.CreateFromSerialized(projectIdGuid)
+                : ProjectId.CreateNewId();
+            var workspace = new AdhocWorkspace();
+            var languageName = GetLanguageName(ProjectPath);
+            workspace.AddProject(ProjectInfo.Create(
+                projectId,
+                VersionStamp.Create(),
+                projectName,
+                projectName,
+                languageName, 
+                ProjectPath,
+                TargetPath,
+                CreateCompilationOptions(OutputType, languageName),
+                documents: GetDocuments(Compile, projectId),
+                metadataReferences: GetMetadataReferences(Reference)
+                ));
             return true;
+        }
+
+        private static IEnumerable<DocumentInfo> GetDocuments(string[] sources, ProjectId projectId) =>
+            sources
+                ?.Where(File.Exists)
+                .Select(x => DocumentInfo.Create(
+                    DocumentId.CreateNewId(projectId),
+                    Path.GetFileName(x),
+                    loader: TextLoader.From(
+                        TextAndVersion.Create(
+                            SourceText.From(File.ReadAllText(x)), VersionStamp.Create())),
+                    filePath: x))
+            ?? Array.Empty<DocumentInfo>();
+
+        private static IEnumerable<MetadataReference> GetMetadataReferences(string[] references) =>
+            references
+                ?.Where(File.Exists)
+                .Select(x => MetadataReference.CreateFromFile(x))
+            ?? (IEnumerable<MetadataReference>)Array.Empty<MetadataReference>();
+
+
+        private static string GetLanguageName(string projectPath)
+        {
+            switch (Path.GetExtension(projectPath))
+            {
+                case ".csproj":
+                    return LanguageNames.CSharp;
+                case ".vbproj":
+                    return LanguageNames.VisualBasic;
+                default:
+                    throw new InvalidOperationException("Could not determine supported language from project path");
+            }
+        }
+
+        private static CompilationOptions CreateCompilationOptions(string outputType, string languageName)
+        {
+            OutputKind? kind = null;
+            switch (outputType)
+            {
+                case "Library":
+                    kind = OutputKind.DynamicallyLinkedLibrary;
+                    break;
+                case "Exe":
+                    kind = OutputKind.ConsoleApplication;
+                    break;
+                case "Module":
+                    kind = OutputKind.NetModule;
+                    break;
+                case "Winexe":
+                    kind = OutputKind.WindowsApplication;
+                    break;
+            }
+
+            if (kind.HasValue)
+            {
+                if (languageName == LanguageNames.CSharp)
+                {
+                    return new CSharpCompilationOptions(kind.Value);
+                }
+                if (languageName == LanguageNames.VisualBasic)
+                {
+                    return new VisualBasicCompilationOptions(kind.Value);
+                }
+            }
+
+            return null;
         }
     }
 }
